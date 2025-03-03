196504293e6b0b45eb0803eb881105bf7430ee25
Bochao Zhang <bochao.zhang@avanade.com>
2025-03-03 09:58:01 +0900

    Deleted unused code

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { MsalProvider } from "@azure/msal-react";
-import { msalInstance } from './auth/msalInstance';
 
 const initializeAndRender = async () => {
-  await msalInstance.clearCache();
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   );
   root.render(
     <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <App />
-      </MsalProvider>
+      <App />
     </React.StrictMode>
   );
 };
